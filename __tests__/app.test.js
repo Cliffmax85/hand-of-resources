27fa9011e1d9614a1bb9fcf5dbbc727bdc7545fc
cliffmax85 <cliffmax85@gmail.com>
2022-03-18 15:14:53 -0700

    passed get all socks test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Sock = require('../lib/models/Sock');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -22,5 +23,13 @@ describe('hand-of-resources routes', () => {
     
     expect(res.body).toEqual({
       id: expect.any(String), ...expected })
-  })
+  });
+
+  it('gets a list of socks', async () => {
+    const expected = await Sock.findAll();
+    const res = await request(app).get('/api/v1/socks');
+    expect(res.body).toEqual(expected);
+  });
+
+  
 });
