34e549c68d939ffcd3241e70079f522f3bc7eb47
cliffmax85 <cliffmax85@gmail.com>
2022-03-18 09:54:39 -0700

    made sql db and connected to beekeeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS socks;
+
+CREATE TABLE socks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    brand TEXT NOT NULL,
+    condition TEXT NOT NULL,
+    is_paired BOOLEAN
+);
+
+INSERT INTO 
+  socks (brand, condition, is_paired)
+VALUES
+  ('kroger', 'tattered', false),
+  ('Champion', 'pristine', true),
+  ('Victorias Secret', 'in ruins', false);
